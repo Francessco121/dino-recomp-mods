5e87a8826a7bab51f3686cd72b955cb57026588f
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-05-04 20:32:39 -0400

    [WIP] new mod_fs api

--- mod_fs/include/mod_fs.h
+++ mod_fs/include/mod_fs.h
@@ -10,6 +10,8 @@
 
 RECOMP_IMPORT("mod_fs", void register_model_replacement(s32 id, void *data, u32 size))
 RECOMP_IMPORT("mod_fs", void register_block_replacement(s32 tabIdx, void *data, u32 size))
+RECOMP_IMPORT("mod_fs", void *get_handle(s32 tabFileId, s32 tabIdx, u32 *outSize))
+RECOMP_IMPORT("mod_fs", void *resize_tab_entry(s32 tabFileId, s32 tabIdx, u32 newSize))
 
 #ifndef _MOD_FS_SRC
 #define MODFS_ON_INIT_CALLBACK RECOMP_CALLBACK("mod_fs", modfs_on_init)
