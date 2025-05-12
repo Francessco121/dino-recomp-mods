725264b27897d97f41318cb09a1eceb2dfe91469
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-05-12 19:41:21 -0400

    Test patch for CapeClaw log

--- shinx121_misc/src/extfs_test.c
+++ shinx121_misc/src/extfs_test.c
@@ -4,6 +4,7 @@
 #include "recomputils.h"
 #include "sys/fs.h"
 #include "sys/map.h"
+#include "sys/map_enums.h"
 
 #include "extfs.h"
 
@@ -17,47 +18,27 @@ EXTFS_ON_LOAD_REPLACEMENTS_CALLBACK void my_extfs_replacements() {
     extfs_tab_set_entry_replacement(BLOCKS_TAB, 643, villagePatch, villagePatch_end - villagePatch);
 }
 
+static void cc_log_patch() {
+    MapHeader *header = extfs_tab_get_entry(MAPS_TAB, MAP_CAPE_CLAW * 7, NULL);
+    void *objects = extfs_tab_get_entry(MAPS_TAB, MAP_CAPE_CLAW * 7 + 4, NULL);
+
+    ObjCreateInfo *objInfo = (ObjCreateInfo*)objects;
+
+    for (s32 i = 0; i < header->objectInstanceCount; i++) {
+        if (objInfo->uID == 11480) {
+            // Swap DFlog with BWLog
+            objInfo->objId = OBJ_BWLog;
+        }
+
+        objInfo = (ObjCreateInfo*)((u32)objInfo + (objInfo->quarterSize << 2));
+    }
+}
+
 EXTFS_ON_LOAD_MODIFICATIONS_CALLBACK void my_extfs_modifications() {
     ObjDef *sabreObjDef = extfs_tab_get_entry(OBJECTS_TAB, 1, NULL);
     u32 *sabreModelList = (u32*)((u32)sabreObjDef + (u32)sabreObjDef->pModelList);
     sabreModelList[0] = 9;
     sabreModelList[1] = 9;
 
-    // u32 capeClawObjects = (29 * 7) + 4;
-    // u32 capeClawObjectsSize;
-    // ObjCreateInfo *capeClawObjInfos = extfs_tab_get_entry(MAPS_TAB, capeClawObjects, &capeClawObjectsSize);
-
-    // MapHeader *header = extfs_tab_get_entry(MAPS_TAB, 29 * 7, NULL);
-
-    // u8 extradata[] = {
-    //     0x0, 0x3 ,0x49 ,0xf3 ,0xff ,0xff ,0xff ,0xff ,0xff ,0xff ,0x0 ,0x0 ,0x0 ,0x0 ,0x0 ,0x0 ,0x0 ,0x0 
-    //     ,0x0 ,0x3 ,0x0 ,0xe ,0x0 ,0x2 ,0x0 ,0x0 ,0xff ,0x1 ,0xff ,0xff ,0xe ,0x0 ,0x0 ,0x0 ,0x0 ,0x0};
-
-    // ObjCreateInfo *obj = extfs_tab_resize_entry(MAPS_TAB, capeClawObjects, 0x50 * 1);
-    // u32 start = (u32)obj;
-
-    // for (s32 i = 0; i < 1; i++) {
-    //     obj->objId = OBJ_BossDrakor;
-    //     obj->quarterSize = 0x50 / 4;
-    //     obj->setup = 0;
-    //     obj->loadParamA = 1;
-    //     obj->loadParamB = 4;
-    //     obj->loadDistance = 0x80;
-    //     obj->fadeDistance = 0x64;
-    //     obj->x = 347.064453f + (3 * i);
-    //     obj->y = 5.390846f + (60 * i);
-    //     obj->z = 959.935547f + (3 * i);
-    //     obj->uID = i;
-
-    //     // u8 *d = (u8*)((u32)obj + sizeof(ObjCreateInfo));
-    //     // for (s32 k = 0; k < 36; k++) {
-    //     //     d[k] = extradata[k];
-    //     // }
-
-    //     obj = (ObjCreateInfo*)((u32)obj + (obj->quarterSize << 2));
-    // }
-
-
-    // header->objectInstanceCount = 1;
-    // header->objectInstancesFileLength = 0x50 * 1;
+    cc_log_patch();
 }
