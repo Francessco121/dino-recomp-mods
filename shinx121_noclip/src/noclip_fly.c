8d436eee06c367c4a8a26363925a21458a692108
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-10-27 02:27:11 -0400

    Make noclip speed independent of framerate

--- shinx121_noclip/src/noclip_fly.c
+++ shinx121_noclip/src/noclip_fly.c
@@ -2,6 +2,7 @@
 #include "recompconfig.h"
 
 #include "common.h"
+#include "dlls/objects/210_player.h"
 #include "sys/controller.h"
 #include "sys/objects.h"
 #include "sys/math.h"
@@ -23,6 +24,8 @@ RECOMP_CALLBACK("*", recomp_on_game_tick) void noclip_fly_cheats_game_tick() {
     Object *player = get_player();
 
     if (player != NULL) {
+        Player_Data *playerdata = (Player_Data*)player->data;
+
         // Noclip fly
         u16 buttons = gContPads[gVirtualContPortMap[0]].button;
         if ((buttons & L_TRIG) && recomp_get_config_u32("noclip") == ALLOW_NOCLIP_ON && player->parent == NULL) {
@@ -30,7 +33,7 @@ RECOMP_CALLBACK("*", recomp_on_game_tick) void noclip_fly_cheats_game_tick() {
                 fly_position = player->srt.transl;
             }
 
-            const float SPEED = 10.0f;
+            const float SPEED = 5.0f;
 
             s8 jx = get_joystick_x(0);
             s8 jy = get_joystick_y(0);
@@ -48,9 +51,9 @@ RECOMP_CALLBACK("*", recomp_on_game_tick) void noclip_fly_cheats_game_tick() {
 
             vec3_normalize(&fly_dir);
             
-            fly_dir.x *= SPEED;
-            fly_dir.y *= SPEED;
-            fly_dir.z *= SPEED;
+            fly_dir.x *= SPEED * gUpdateRateF;
+            fly_dir.y *= SPEED * gUpdateRateF;
+            fly_dir.z *= SPEED * gUpdateRateF;
 
             SRT cam_srt = {
                 .transl.x = 0,
@@ -76,14 +79,14 @@ RECOMP_CALLBACK("*", recomp_on_game_tick) void noclip_fly_cheats_game_tick() {
             player->positionMirror2 = fly_position;
             player->positionMirror3 = fly_position;
 
-            *((u16*)((u32)player + 0x0360)) = 28; // anim
+            playerdata->unk0.animState = 28;
             player->unk36 = 0x80;
             player->animProgress = 0.0f;
 
             flying_cooldown = FLY_COOLDOWN;
         } else if (flying_cooldown > 0) {
             flying_cooldown--;
-            *((u16*)((u32)player + 0x0360)) = 1;
+            playerdata->unk0.animState = 1;
             player->animProgress = 0.0f;
             player->unk36 = 0xFF;
         }
