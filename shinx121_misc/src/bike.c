758403467f127729c1939a310e25af9b2cf56443
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-11-11 02:31:09 -0500

    Make bike cheat way safer

--- shinx121_misc/src/bike.c
+++ shinx121_misc/src/bike.c
@@ -11,6 +11,7 @@
 #include "sys/memory.h"
 #include "sys/map.h"
 #include "sys/objects.h"
+#include "sys/objtype.h"
 #include "common.h"
 #include "functions.h"
 #include "dll.h"
@@ -18,7 +19,33 @@
 static s32 windowOpen = 0;
 static Object *bike;
 
+static Object *validate_bike(void) {
+    s32 objListCount;
+    Object **objList = obj_get_all_of_type(0xB, &objListCount);
+    for (s32 i = 0; i < objListCount; i++) {
+        if (objList[i] == bike) {
+            return objList[i];
+        }
+    }
+
+    // Must have been destroyed
+    bike = NULL;
+    return NULL;
+}
+
+static s32 is_player_on_bike(void) {
+    Object *bike = validate_bike();
+
+    Object *player = get_player();
+    Player_Data *playerdata = (Player_Data*)player->data;
+
+    return bike != NULL && playerdata->unk858 == bike;
+}
+
 static void destroy_bike(void) {
+    if (is_player_on_bike()) return;
+    validate_bike();
+
     if (bike != NULL) {
         obj_destroy_object(bike);
         bike = NULL;
@@ -26,6 +53,7 @@ static void destroy_bike(void) {
 }
 
 static void spawn_bike(void) {
+    if (is_player_on_bike()) return;
     destroy_bike();
 
     Object *player = get_player();
@@ -35,9 +63,9 @@ static void spawn_bike(void) {
     setup->fadeFlags = 4;
     setup->loadDistance = 1;
     setup->fadeDistance = 99;
-    setup->x = player->srt.transl.x;
-    setup->y = player->srt.transl.y;
-    setup->z = player->srt.transl.z;
+    setup->x = player->positionMirror.x;
+    setup->y = player->positionMirror.y;
+    setup->z = player->positionMirror.z;
     *((u32*)((u32)setup + 0x18)) = 0xB800FFFF;
     *((u32*)((u32)setup + 0x1C)) = 0x0001FFFF;
     *((u32*)((u32)setup + 0x20)) = 0x1C000000;
@@ -52,12 +80,15 @@ RECOMP_CALLBACK(".", my_cheats_menu_event) void bike_cheats_menu_callback() {
 RECOMP_CALLBACK(".", my_dbgui_event) void bike_dbgui_callback() {
     if (windowOpen) {
         if (dbgui_begin("Bike", &windowOpen)) {
-            if (dbgui_button("Spawn Bike")) {
-                spawn_bike();
-            }
-
-            if (dbgui_button("Destroy Bike")) {
-                destroy_bike();
+            Object *bike = validate_bike();
+            if (bike == NULL) {
+                if (dbgui_button("Spawn Bike")) {
+                    spawn_bike();
+                }
+            } else {
+                if (dbgui_button("Destroy Bike")) {
+                    destroy_bike();
+                }
             }
         }
         dbgui_end();
@@ -74,6 +105,8 @@ RECOMP_CALLBACK("*", recomp_on_game_tick) void test_game_tick() {
     //         }
     //     }
     // }
+
+    Object *bike = validate_bike();
 
     if (bike != NULL) {
         void *objdata = bike->data;
