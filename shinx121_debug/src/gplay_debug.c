ac84d17d07914ad3db1085ca185dda2ad7368925
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-05-29 19:50:28 -0400

    [debug] gplay timesave editor

--- shinx121_debug/src/gplay_debug.c
+++ shinx121_debug/src/gplay_debug.c
@@ -47,6 +47,24 @@ static void saved_objects_tab(GameState* state) {
     dbgui_end_child();
 }
 
+static void time_saves_tab(GameState* state) {
+    if (dbgui_begin_child("scroll")) {
+        for (s32 i = 0; i < state->save.file.timeSaveCount; i++) {
+            TimeSave* timeSave = &state->save.file.timeSaves[i];
+            dbgui_push_str_id(recomp_sprintf_helper("%d", i));
+            dbgui_textf("timeSaves[%d]:", i);
+            dbgui_same_line();
+            if (dbgui_button("Delete")) {
+                timeSave->time = 0;
+            }
+            dbgui_textf("  UID 0x%X", timeSave->uid);
+            dbgui_textf("  Time: %.0f (%.0f left)", timeSave->time, timeSave->time - state->save.file.timePlayed);
+            dbgui_pop_id();
+        }
+    }
+    dbgui_end_child();
+}
+
 RECOMP_CALLBACK(".", my_debug_menu_event) void gplay_debug_menu_callback() {
     dbgui_menu_item("Gplay", &windowOpen);
 }
@@ -58,6 +76,10 @@ RECOMP_CALLBACK(".", my_dbgui_event) void gplay_debug_dbgui_callback() {
             if (dbgui_begin_tab_bar("tabs")) {
                 if (dbgui_begin_tab_item("Saved Objects", NULL)) {
                     saved_objects_tab(state);
+                    dbgui_end_tab_item();
+                }
+                if (dbgui_begin_tab_item("Time Saves", NULL)) {
+                    time_saves_tab(state);
                     dbgui_end_tab_item();
                 }
                 dbgui_end_tab_bar();
